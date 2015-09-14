4669bf0dac1effee00435549fe44f560523b1be8
Ludovic Ferret <ludovic_ferret@yahoo.com>
2015-09-14 15:23:34 -0700

    some java

---
+++ src/com/ludo/ppx/Counter.java
@@ -0,0 +1,30 @@
+package com.ludo.ppx;
+
+public class Counter {
+
+    private int i;
+
+    public Counter() {
+        this(0);
+    }
+
+    public Counter(int i) {
+        this.i = i;
+    }
+
+    public void inc() {
+        i++;
+    }
+
+    public void dec() {
+        i--;
+    }
+
+    public int value() {
+        return i;
+    }
+
+    public boolean isDone() {
+        return i == 0;
+    }
+}
